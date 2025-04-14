39457ef01bf15d4acc91684c69c407e710772e0f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-14 11:46:05 +0600

    Successfully solved even number related problem
    This problem was solved in codeforces

---
+++ B_Even_Numbers.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num, i, count = 0;
+
+//     scanf("%d", &num);
+
+//     for (i = 1; i <= num; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             printf("%d\n", i);
+//             count++;
+//         }
+//     }
+//     if (count == 0)
+//     {
+//         printf("-1");
+//     }
+
+//     return 0;
+// }

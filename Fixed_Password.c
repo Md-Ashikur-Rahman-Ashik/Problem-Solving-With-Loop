eb9e13b8fddc15a178336671e3d05c4568142c55
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-14 13:19:25 +0600

    Trying to solve fixed password problem

---
+++ Fixed_Password.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+
+    scanf("%d\n", &num1);
+    scanf("%d\n", &num2);
+    scanf("%d\n", &num3);
+    scanf("%d\n", &num4);
+    scanf("%d\n", &num5);
+    scanf("%d\n", &num6);
+    scanf("%d\n", &num7);
+    scanf("%d\n", &num8);
+    scanf("%d\n", &num9);
+    scanf("%d", &num10);
+
+    if (num1 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num2 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num3 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num4 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num5 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num6 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num7 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num8 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num9 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    if (num10 == 1999)
+    {
+        /* code */
+        printf("Correct");
+        return 0;
+    }
+    else
+    {
+        printf("Wrong\n");
+    }
+
+    return 0;
+}

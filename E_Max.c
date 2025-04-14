1b74504caa9fed4e41625e0a0be242dac4850347
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-14 22:43:56 +0600

    Solved max problem in codeforces

---
+++ E_Max.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int count, num = 0, i, max = 0;
+
+//     scanf("%d", &count);
+
+//     for (i = 1; i <= count; i++)
+//     {
+//         /* code */
+//         scanf("%d", &num);
+
+//         if (max < num)
+//         {
+//             max = num;
+//         }
+//     }
+
+//     printf("%d", max);
+
+//     return 0;
+// }

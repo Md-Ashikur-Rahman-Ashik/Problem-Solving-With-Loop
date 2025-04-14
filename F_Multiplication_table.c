cbe9e595a135325b031179ad534d4ba4f4661cbd
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-14 22:55:47 +0600

    Solved multiplication table on codeforces

---
+++ F_Multiplication_table.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num, i;
+
+//     scanf("%d", &num);
+
+//     for (i = 1; i <= 12; i++)
+//     {
+//         /* code */
+//         int multiply = num * i;
+//         printf("%d * %d = %d\n", num, i, multiply);
+//     }
+
+//     return 0;
+// }

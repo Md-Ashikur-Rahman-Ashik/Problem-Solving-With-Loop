bdacb9106746156f33d7b9961db53bdde53b4c53
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-14 22:19:49 +0600

    Successfully solved fixed password problem
    This problem was solved for codeforces

--- Fixed_Password.c
+++ Fixed_Password.c
@@ -1,129 +1,180 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+// int main()
+// {
+//     int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
 
-    scanf("%d\n", &num1);
-    scanf("%d\n", &num2);
-    scanf("%d\n", &num3);
-    scanf("%d\n", &num4);
-    scanf("%d\n", &num5);
-    scanf("%d\n", &num6);
-    scanf("%d\n", &num7);
-    scanf("%d\n", &num8);
-    scanf("%d\n", &num9);
-    scanf("%d", &num10);
+//     scanf("%d\n", &num1);
+//     scanf("%d\n", &num2);
+//     scanf("%d\n", &num3);
+//     scanf("%d\n", &num4);
+//     scanf("%d\n", &num5);
+//     scanf("%d\n", &num6);
+//     scanf("%d\n", &num7);
+//     scanf("%d\n", &num8);
+//     scanf("%d\n", &num9);
+//     scanf("%d", &num10);
 
-    if (num1 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num1 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num2 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num2 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num3 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num3 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num4 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num4 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num5 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num5 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num6 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num6 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num7 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num7 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num8 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num8 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num9 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num9 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    if (num10 == 1999)
-    {
-        /* code */
-        printf("Correct");
-        return 0;
-    }
-    else
-    {
-        printf("Wrong\n");
-    }
+//     if (num10 == 1999)
+//     {
+//         /* code */
+//         printf("Correct");
+//         return 0;
+//     }
+//     else
+//     {
+//         printf("Wrong\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num = 5, i;
+
+//     for (i = 0; i <= num; i++)
+//     {
+//         /* code */
+//         int num_1 = 'a' + i;
+
+//         scanf("%d\n", &num_1);
+//         // printf("%d", num_1);
+
+//         if (num_1 == 1999)
+//         {
+//             printf("Correct");
+//         }
+//         else
+//         {
+//             printf("Wrong\n");
+//         }
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int pass;
+
+//     while (scanf("%d", &pass))
+//     {
+//         /* code */
+//         if (pass == 1999)
+//         {
+//             printf("Correct");
+
+//             break;
+//             ;
+//         }
+//         else
+//         {
+//             printf("Wrong\n");
+//         }
+//     }
+
+//     return 0;
+// }

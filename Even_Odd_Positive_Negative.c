561682f77ddfb94c15c417b7d9e78f69e49b79a8
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-14 12:44:54 +0600

    Solved even, odd, positive, negative problem
    This problem was solved in codeforces

---
+++ Even_Odd_Positive_Negative.c
@@ -0,0 +1,41 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num, i, even_count = 0, odd_count = 0, positive_count = 0, negative_count = 0;
+
+//     scanf("%d", &num);
+
+//     for (i = 1; i <= num; i++)
+//     {
+//         int num_1 = 'a' + i;
+//         scanf("%d", &num_1);
+
+//         if (num_1 % 2 == 0)
+//         {
+//             even_count++;
+//         }
+
+//         if (num_1 % 2 != 0)
+//         {
+//             odd_count++;
+//         }
+
+//         if (num_1 > 0)
+//         {
+//             positive_count++;
+//         }
+
+//         if (num_1 < 0)
+//         {
+//             negative_count++;
+//         }
+//     }
+
+//     printf("Even: %d\n", even_count);
+//     printf("Odd: %d\n", odd_count);
+//     printf("Positive: %d\n", positive_count);
+//     printf("Negative: %d", negative_count);
+
+//     return 0;
+// }

9b92c787715d0862ce52ed557910127562649886
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-15 13:29:18 +0600

    Solved first practice problem

---
+++ Practice_Problems.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 3;
+//     while (i <= 5)
+//     {
+//         /* code */
+//         for (int j = 0; j < i; j++)
+//         {
+//             /* code */
+//             printf("*");
+//         }
+//         i++;
+//     }
+
+//     return 0;
+// }
